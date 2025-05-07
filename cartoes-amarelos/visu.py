06cf0e254e700de958dfa157c477113208778b62
Iure Santiago <iuresantiag016@gmail.com>
2025-05-07 17:02:21 -0300

    graficos comissao e tecnicos mais indisciplinados

--- cartoes-amarelos/visu.py
+++ cartoes-amarelos/visu.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 # %%
 
-amarelos_path = 'data/sa25_06_camarelos.parquet'
+amarelos_path = '../data/sa24_06_camarelos.parquet'
 
 df_amarelos = pd.read_parquet(amarelos_path)
 df_amarelos
@@ -27,7 +27,7 @@ df_amarelos[df_amarelos['jogo'] == 1]
 df_amarelos[df_amarelos['jogo'] == 1] #? como posso passar mais um parametro?
 
 # %%
-jogos_path = 'data/sa25_01_jogos.parquet'
+jogos_path = '../data/sa25_01_jogos.parquet'
 df_jogos = pd.read_parquet(jogos_path, columns=['competicao', 'comp', 'ano', 'rodada', 'jogo', 'mandante', 'visitante'])
 df_jogos
 
@@ -88,7 +88,7 @@ janela = plt.figure(figsize=(10,5))
 grafico = janela.add_axes([0,0,1,1])
 bars = grafico.bar(amarelos_rodada_j['rodada'], amarelos_rodada_j['amarelo_rodada_j'])
 
-plt.title('Cartoes amarelos por rodada no Brasileirao 2024', fontsize=15, fontweight="bold")
+plt.title('Cartoes Amarelos por Rodada no Brasileirao 2024', fontsize=15, fontweight="bold")
 plt.xlabel('Numero da Rodada')
 plt.ylabel('Quantia de Cartoes')
 
@@ -125,13 +125,14 @@ for bar in bars:
     width = bar.get_width()
     grafico.annotate(f'{width}',
                      xy=(width, bar.get_y() + bar.get_height()/2),
-                     xytext=(3, 0),  # Deslocamento do texto
+                     xytext=(3, 0),
                      textcoords="offset points",
                      ha='left', va='center')
 
 plt.show()
 # %%
 #* Filtrando por NOME, quais foram os jogadores mais amarelados
+# todo melhorar, colocar time de cada jogador e talvez seu apelido
 amarelos_nome = (df_aaj
                  .groupby(by='nome_jogador', as_index=False)
                  .agg(amarelos_nome = ('nome_jogador', 'count'))
@@ -153,8 +154,108 @@ plt.xlabel('Quantia de Cartoes')
 plt.ylabel('Nome do Jogador')
 
 plt.show()
-# %%
-# # TODO Comissao tecnica mais indisciplinada 
-# Mostrar por time as comissoes que mais recebem cartoes amarelos
+#%%
+comissao_path = '../data/sa25_04_comissaotec.parquet'
+df_comissaotec = pd.read_parquet(comissao_path)
+df_comissaotec
 
-df_merge['n_cbf'].isna()
+# %%
+# mantendo apenas os dados de individuos que nao tem n_cbf, ou seja, integrantes da comissao tecnica 
+
+df_aac = df_merge[df_merge['n_cbf'].isna()]
+df_aac
+
+# %%
+df_aac = df_aac.rename(columns={'nome_jogador' : 'nome'})
+df_aac
+
+# %% 
+
+df_aac.dropna(subset=['time'], inplace=True)
+
+# %%
+
+df_aac[df_aac['time'].str.startswith("Bah")]
+# %% fazendo merge entre os dfs de amarelos apenas para comissao e da comissao tecnica 
+
+df_amarelos_comissao = df_aac.merge(df_comissaotec,
+                                              how='left',
+                                              on=['comp', 'ano', 'jogo', 'time', 'nome'])
+
+#tirando colunas que os valores sao desnecessarios/vazios
+df_amarelos_comissao = df_amarelos_comissao.drop(columns=['m/v_x', 'T/R', 'n_cbf'])
+df_amarelos_comissao
+
+# %% 
+df_amarelos_comissao.info()
+
+# %%
+amarelos_comissao = (df_amarelos_comissao
+                .groupby(by='time', as_index=False)
+                .agg(amarelos_comissao = ('time', 'count'))
+                .sort_values('amarelos_comissao', ascending=True)
+        )
+amarelos_comissao
+
+# 154 cartoes totais
+# %% Grafico de Comissao Tecnica Mais Indisciplinadas (por time comissoes q + recebem cartoes amarelos)
+janela = plt.figure(figsize=(10,5))
+grafico = janela.add_axes([0,0,1,1])
+bars = grafico.barh(amarelos_comissao['time'], amarelos_comissao['amarelos_comissao'])
+
+plt.title("Comissoes Tecnicas Mais Indisciplinadas", fontsize=15, fontweight="bold")
+plt.xlabel("Quantia de Cartoes")
+plt.ylabel("Times")
+
+for bar in bars:
+    width = bar.get_width()
+    grafico.annotate(f'{width}',
+                     xy=(width, bar.get_y() + bar.get_height()/2),
+                     xytext=(3, 0),
+                     textcoords="offset points",
+                     ha='left', va='center')
+
+plt.show()
+
+# %%
+amarelos_funcao = (df_amarelos_comissao
+                   .groupby(by='funcao', as_index=False)
+                   .agg(amarelos_funcao = ('funcao', 'count'))
+                   .sort_values('amarelos_funcao', ascending=True)
+                   )
+amarelos_funcao
+# 134 cartoes totais
+# %% tabela dos tecnicos mais amarelados 
+df_apenas_tecnicos = df_amarelos_comissao[df_amarelos_comissao['funcao'] == 'Técnico']
+df_apenas_tecnicos
+# %%
+
+df_apenas_tecnicos['nome'].value_counts()
+
+# %%
+amarelos_tecnicos = (df_apenas_tecnicos
+                   .groupby(by='nome', as_index=False)
+                   .agg(amarelos_tecnicos = ('nome', 'count'))
+                   .sort_values('amarelos_tecnicos', ascending=True)
+                   )
+amarelos_tecnicos
+# %% grafico dos tecnicos mais amarelados em 2024
+
+janela = plt.figure(figsize=(10,5))
+grafico = janela.add_axes([0,0,1,1])
+bars = grafico.barh(amarelos_tecnicos['nome'],amarelos_tecnicos['amarelos_tecnicos'])
+
+plt.title('Os Tecnicos mais Amarelados no Brasileirao 2024', fontsize=15, fontweight='bold')
+plt.xlabel('Quantia de Cartoes')
+plt.ylabel('Nome')
+
+for bar in bars:
+    width = bar.get_width()
+    grafico.annotate(f'{width}',
+                     xy=(width, bar.get_y() + bar.get_height()/2),
+                     xytext=(3, 0),
+                     textcoords="offset points",
+                     ha='left', va='center')
+
+plt.show()
+# %%
