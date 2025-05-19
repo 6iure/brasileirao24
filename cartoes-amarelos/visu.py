fddee92870d574269fc520b222a93c477d74ce1b
Iure Santiago <iuresantiag016@gmail.com>
2025-05-19 10:53:36 -0300

    melhorias visuais nos graficos

--- cartoes-amarelos/visu.py
+++ cartoes-amarelos/visu.py
@@ -3,6 +3,7 @@ import pandas as pd
 pd.plotting.register_matplotlib_converters()
 import matplotlib.pyplot as plt
 import seaborn as sns
+from matplotlib import colormaps
 # %%
 
 amarelos_path = '../data/sa24_06_camarelos.parquet'
@@ -82,26 +83,37 @@ plt.xlim(1,38)
 plt.xticks(range(1,39, 1))
 sns.barplot(data=amarelos_rodada_j, x='rodada', y='amarelo_rodada_j')
 # %% 
-# * grafico usando apenas MATPLOTLIB
-
+# * Cartoes amarelos por Rodada no Brasileirao
 janela = plt.figure(figsize=(10,5))
 grafico = janela.add_axes([0,0,1,1])
-bars = grafico.bar(amarelos_rodada_j['rodada'], amarelos_rodada_j['amarelo_rodada_j'])
+colors = plt.cm.Wistia(0.2 + 0.7 * amarelos_rodada_j['amarelo_rodada_j'] / max(amarelos_rodada_j['amarelo_rodada_j']))
 
-plt.title('Cartoes Amarelos por Rodada no Brasileirao 2024', fontsize=15, fontweight="bold")
-plt.xlabel('Numero da Rodada')
-plt.ylabel('Quantia de Cartoes')
+bars = grafico.bar(
+        amarelos_rodada_j['rodada'],
+        amarelos_rodada_j['amarelo_rodada_j'],
+        color = colors,
+        linewidth=1.2,
+        alpha=0.8,
+)
+
+plt.title('Cartoes Amarelos por Rodada - Brasileirao 2024', fontsize=16, fontweight="bold")
+plt.xlabel('Rodada', fontsize=12, color='#555', labelpad=10)
+plt.ylabel('Total de Cartoes', fontsize=12, labelpad=10, color='#555')
 
 grafico.set_xlim(0.5, 38.5)  # Margem para melhor visualização
-grafico.set_xticks(range(1, 38, 4))  # Força a exibição de todas as rodadas (1 a 38)
+grafico.set_xticks(range(1, 39, 4))
+grafico.grid(axis='y', linestyle=':', alpha=0.4, color='gray')
+grafico.set_axisbelow(True)
 
+# numero exato em cima de cada barra
 for bar in bars:
     height = bar.get_height()
     grafico.annotate(f'{height}',
-                     xy=(bar.get_x() + bar.get_width() / 2, height),  # Posição no topo da barra
-                     xytext=(0, 2),  # Deslocamento vertical do texto (2 pontos para cima)
+                     xy=(bar.get_x() + bar.get_width() / 2, height),  
+                     xytext=(0, 2),  
                      textcoords="offset points",
-                     ha='center', va='bottom')  # Alinhamento centralizado na base
+                     ha='center',
+                     va='bottom') 
 plt.show()
 # %% 
 #* Cartoes amarelos por time (Apenas para jogadores)
@@ -112,29 +124,48 @@ amarelos_time = (df_aaj
         )
 amarelos_time   
 # %%
-#* grafico em barras verticais de quantos amarelos cada time recebeu (deixar mais bonitinho)
-janela = plt.figure(figsize=(10,5))
+#* grafico em barras verticais de quantos amarelos cada time recebeu
+janela = plt.figure(figsize=(10,8), facecolor='white')
 grafico = janela.add_axes([0,0,1,1])
-bars = grafico.barh(amarelos_time['time'], amarelos_time['amarelos_time'])
+colors = plt.cm.Wistia(0.1 + 0.9 * amarelos_time['amarelos_time'] / max(amarelos_time['amarelos_time']))
 
-plt.title("Cartoes Amarelos por Time no Brasileirao 2024", fontsize=15, fontweight="bold")
-plt.xlabel("Quantia de Cartoes")
-plt.ylabel("Times")
+bars = grafico.barh(
+    amarelos_time['time'],
+    amarelos_time['amarelos_time'],
+    color = colors,
+    linewidth=1.2,
+    alpha=0.8
+    )
+
+plt.title("Cartoes Amarelos por Time - Brasileirao 2024", fontsize=15, fontweight="bold",)
+plt.xlabel("Total de Cartoes (Apenas Jogadores)",fontsize=12, color='#555' )
+plt.ylabel("Times", fontsize=12, color='#555')
+
+grafico.grid(axis='x', linestyle='--', alpha=0.4, color='#cccccc')
+grafico.set_axisbelow(True)
+
+for spine in ['top', 'right', 'bottom']:
+    grafico.spines[spine].set_visible(False)
 
 for bar in bars:
     width = bar.get_width()
-    grafico.annotate(f'{width}',
-                     xy=(width, bar.get_y() + bar.get_height()/2),
-                     xytext=(3, 0),
+    grafico.annotate(f'{width:.0f}',
+                     xy=(width -1, bar.get_y() + bar.get_height()/2),
+                     xytext=(0, 0),
                      textcoords="offset points",
-                     ha='left', va='center')
+                     ha='right',
+                     va='center',
+                     fontsize=10,
+                     color='white',
+                     fontweight='bold'
+                     )
 
 plt.show()
 # %%
 #* Filtrando por NOME, quais foram os jogadores mais amarelados
 # todo melhorar, colocar time de cada jogador e talvez seu apelido
 amarelos_nome = (df_aaj
-                 .groupby(by='nome_jogador', as_index=False)
+                 .groupby(by=['nome_jogador', 'time'], as_index=False)
                  .agg(amarelos_nome = ('nome_jogador', 'count'))
                  .sort_values('amarelos_nome', ascending=True)
                  )
@@ -143,15 +174,41 @@ amarelos_nome
 # %%
 mais_amarelados = amarelos_nome.tail(10)
 mais_amarelados
-# %%
+# %% Grafico de jogadores com mais amarelos 
+#todo adicionar time de cada jogador dentro da barra 
 
 janela = plt. figure(figsize=(10, 5))
 grafico = janela.add_axes([0,0,1,1])
-grafico.barh(mais_amarelados['nome_jogador'], mais_amarelados['amarelos_nome'])
+colors = plt.cm.Wistia(0.2 + 0.7 * amarelos_time['amarelos_time'] / max(amarelos_time['amarelos_time']))
+bars = grafico.barh(
+    mais_amarelados['nome_jogador'], 
+    mais_amarelados['amarelos_nome'],
+    color = colors,
+    alpha = 0.8
+)
 
-plt.title('Jogadores que Mais Receberam Cartoes Amarelos no Brasileirao 2024', fontsize=15, fontweight="bold")
-plt.xlabel('Quantia de Cartoes')
-plt.ylabel('Nome do Jogador')
+plt.title('Jogadores que Mais Receberam Cartoes Amarelos - Brasileirao 2024', fontsize=15, fontweight="bold")
+plt.xlabel('Total de Cartoes', fontsize=12, color='#555')
+plt.ylabel('Nome do Jogador', fontsize=12, color='#555')
+
+grafico.grid(axis='x', linestyle='--', alpha=0.4, color='#cccccc')
+grafico.set_axisbelow(True)
+
+for spine in ['top', 'right', 'bottom']:
+    grafico.spines[spine].set_visible(False)
+
+for bar in bars:
+    width = bar.get_width()
+    grafico.annotate(f'{width:.0f}',
+                     xy=(width -0.1, bar.get_y() + bar.get_height()/2),
+                     xytext=(0, 0),
+                     textcoords="offset points",
+                     ha='right',
+                     va='center',
+                     fontsize=10,
+                     color='white',
+                     fontweight='bold'
+                     )
 
 plt.show()
 #%%
@@ -161,7 +218,6 @@ df_comissaotec
 
 # %%
 # mantendo apenas os dados de individuos que nao tem n_cbf, ou seja, integrantes da comissao tecnica 
-
 df_aac = df_merge[df_merge['n_cbf'].isna()]
 df_aac
 
@@ -189,7 +245,7 @@ df_amarelos_comissao
 # %% 
 df_amarelos_comissao.info()
 
-# %%
+# %% filtrando por apenas time e amarelos por comissao
 amarelos_comissao = (df_amarelos_comissao
                 .groupby(by='time', as_index=False)
                 .agg(amarelos_comissao = ('time', 'count'))
@@ -198,25 +254,42 @@ amarelos_comissao = (df_amarelos_comissao
 amarelos_comissao
 
 # 154 cartoes totais
-# %% Grafico de Comissao Tecnica Mais Indisciplinadas (por time comissoes q + recebem cartoes amarelos)
+# %% Grafico de Comissoes Tecnicas Mais Indisciplinadas (por time comissoes q + recebem cartoes amarelos)
 janela = plt.figure(figsize=(10,5))
 grafico = janela.add_axes([0,0,1,1])
-bars = grafico.barh(amarelos_comissao['time'], amarelos_comissao['amarelos_comissao'])
+colors = plt.cm.Wistia(0.2 + 0.7 * amarelos_comissao['amarelos_comissao'] / max(amarelos_comissao['amarelos_comissao']))
 
-plt.title("Comissoes Tecnicas Mais Indisciplinadas", fontsize=15, fontweight="bold")
-plt.xlabel("Quantia de Cartoes")
-plt.ylabel("Times")
+bars = grafico.barh(
+    amarelos_comissao['time'],
+    amarelos_comissao['amarelos_comissao'],
+    color = colors,
+    alpha = 0.8
+    )
+
+plt.title("Comissoes Tecnicas Mais Indisciplinadas - Brasileirao 2024", fontsize=15, fontweight="bold")
+plt.xlabel("Total de Cartoes", fontsize=12, color='#555')
+plt.ylabel("Times", fontsize=12, color='#555')
+
+grafico.grid(axis='x', linestyle='--', alpha=0.4, color='#CCCCCC')
+grafico.set_axisbelow(True)
+
+for spine in ['top', 'right', 'bottom']:
+    grafico.spines[spine].set_visible(False)
 
 for bar in bars:
     width = bar.get_width()
-    grafico.annotate(f'{width}',
-                     xy=(width, bar.get_y() + bar.get_height()/2),
-                     xytext=(3, 0),
-                     textcoords="offset points",
-                     ha='left', va='center')
+    grafico.annotate(
+        f'{width:.0f}',
+        xy=(width, bar.get_y() + bar.get_height()/2),
+        xytext=(2, 0),
+        textcoords="offset points",
+        ha='left',
+        va='center',
+        color='black',
+        fontweight='bold',
+    )
 
 plt.show()
-
 # %%
 amarelos_funcao = (df_amarelos_comissao
                    .groupby(by='funcao', as_index=False)
@@ -240,22 +313,37 @@ amarelos_tecnicos = (df_apenas_tecnicos
                    )
 amarelos_tecnicos
 # %% grafico dos tecnicos mais amarelados em 2024
+#todo adicionar o time de cada tecnico
+janela = plt.figure(figsize=(12, 8))
+grafico = janela.add_axes([0.1, 0.2, 0.75, 0.7])
+colors = plt.cm.Wistia(0.2 + 0.7 * amarelos_tecnicos['amarelos_tecnicos'] / max(amarelos_tecnicos['amarelos_tecnicos']))
+bars = grafico.barh(
+    amarelos_tecnicos['nome'],
+    amarelos_tecnicos['amarelos_tecnicos'],
+    color=colors,
+    alpha=0.8
+    )
 
-janela = plt.figure(figsize=(10,5))
-grafico = janela.add_axes([0,0,1,1])
-bars = grafico.barh(amarelos_tecnicos['nome'],amarelos_tecnicos['amarelos_tecnicos'])
+plt.title('Tecnicos Mais Indisciplinados - Brasileirao 2024', fontsize=15, fontweight='bold')
+plt.xlabel('Total de Cartoes', fontsize=12, color='#555')
+plt.ylabel('Nome', fontsize=12, color='#555')
 
-plt.title('Os Tecnicos mais Amarelados no Brasileirao 2024', fontsize=15, fontweight='bold')
-plt.xlabel('Quantia de Cartoes')
-plt.ylabel('Nome')
+grafico.grid(axis='x', linestyle='--', alpha=0.4, color='#CCCCCC')
+grafico.set_axisbelow(True)
+
+for spine in ['top', 'right', 'bottom']:
+    grafico.spines[spine].set_visible(False)
 
 for bar in bars:
     width = bar.get_width()
     grafico.annotate(f'{width}',
                      xy=(width, bar.get_y() + bar.get_height()/2),
-                     xytext=(3, 0),
+                     xytext=(2, 0),
                      textcoords="offset points",
-                     ha='left', va='center')
+                     ha='left',
+                     va='center',
+                     fontweight='bold'
+                     )
 
 plt.show()
 # %%
